a3eed68d20d768b75a91369a02aa131b3eb13078
nathy <nathalia.cericatto@gmail.com>
2024-10-21 16:41:33 -0300

    herança

---
+++ Heranca/SavingAccount.java
@@ -0,0 +1,23 @@
+package Heranca;
+
+public class SavingAccount extends Acciount{
+    private double intersrtRate;
+
+    public SavingAccount(Integer number, String holder, Double balance, double intersrtRate) {
+        super(number, holder, balance);
+        this.intersrtRate = intersrtRate;
+
+    }
+
+    public double getIntersrtRate() {
+        return intersrtRate;
+    }
+
+    public void setIntersrtRate(double intersrtRate) {
+        this.intersrtRate = intersrtRate;
+    }
+
+    public void updateBalance(){
+        balance += intersrtRate;
+    }
+}
