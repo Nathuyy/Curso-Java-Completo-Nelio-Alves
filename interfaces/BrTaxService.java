fe8d84c657415664f4b380bb434a0c836ca2524f
nathy <nathalia.cericatto@gmail.com>
2024-10-28 15:18:51 -0300

    interfaces

---
+++ interfaces/BrTaxService.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public class BrTaxService implements TaxService{
+    @Override
+    public double tax(double amount) {
+        return 0;
+    }
+
+    //logia para a taxa
+}
---
+++ interfaces/Main.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        TaxService taxService; //ao inves de chamar a classe, vai chamar a interface
+    }
+}
