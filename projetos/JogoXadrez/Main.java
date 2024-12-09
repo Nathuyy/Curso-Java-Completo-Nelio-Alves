22bd6fd09c219743d51d6f75e5da2d92c7683dba
nathy <nathalia.cericatto@gmail.com>
2024-12-09 20:01:33 -0300

    camada chess

--- projetos/JogoXadrez/Main.java
+++ projetos/JogoXadrez/Main.java
@@ -1,9 +1,12 @@
 package projetos.JogoXadrez;
 
 import projetos.JogoXadrez.boardGame.Board;
+import projetos.JogoXadrez.chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
         Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
